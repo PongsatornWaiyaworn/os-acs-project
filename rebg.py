288b2e9a804c1cd25dbed77788439f80242b433f
PongsatornWaiyaworn <117834631+PongsatornWaiyaworn@users.noreply.github.com>
2024-10-10 23:04:30 +0700

    test

--- rebg.py
+++ rebg.py
@@ -1,13 +1,1 @@
-from rembg import remove
-from PIL import Image
-
-input_path = 'tee.png'
-output_path = 'output_image.png'
-
-input_image = Image.open(input_path)
-
-output_image = remove(input_image)
-
-output_image.save(output_path)
-
-print("การลบพื้นหลังเสร็จสิ้น")
+print("hello, everyone สามารถมาแก้ได้ครับ เผื่ออยากลองว่ามันใช้ได้ไหม")
