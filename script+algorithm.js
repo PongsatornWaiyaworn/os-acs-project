1fcce9810d8b892140cdb36706b52afa945c7f97
PongsatornWaiyaworn <117834631+PongsatornWaiyaworn@users.noreply.github.com>
2024-10-13 07:50:29 +0700

    FCFS+โครงสร้างข้อมูล

--- script+algorithm.js
+++ script+algorithm.js
@@ -1,9 +1,9 @@
 
 let processes = []; //ex. [{p1, 0, 5, 1}, {p2, 1, 8, 0}] >> {name, arrivalTime, burstTime, priority}
-let result_FCFS = []; //ex. [{p1, 5, 5, 0}, {p2, 13, 12, 4}] >> {name, completionTime, turnAroundTime, waitingTime}
+let result_FCFS = []; //ex. [{p1, 0, 5, 5, 5, 0}, {p2, 1, 8,13, 12, 4}] >> {name, arrivalTime, burstTime, completionTime, turnAroundTime, waitingTime}
 let timeline_FCFS = []; //ex. [{p1, 0, 5}, {p2, 5, 13}] >> {name, start, end}
 let result_RR = [];
-let timeline_RR = []; //ex. [{p1, 0, 4}, {p2, 4, 8}, {p1, 8, 9}, {p2, 8, 12}] สมมุติ timequantum = 4
+let timeline_RR = []; //ex. [{p1, 0, 4}, {p2, 4, 8}, {p1, 8, 9}, {p2, 8, 12}] >> สมมุติ timequantum = 4
 let result_SJF = [];
 let timeline_SJF = [];
 let result_SRTF = [];
@@ -45,5 +45,5 @@ function FCFS() {
         timeline_FCFS.push({ name: process.name, start: startTime, end: completionTime });
 
         currentTime = completionTime;
-    }); 
+    });
 }
